b386517f26c48bc377af7dd19236fae873b7f369
Chad Palmer <chad.n.palmer@gmail.com>
2022-03-28 18:21:02 -0500

    temporary state added to user form with some minor changes

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,17 +1,41 @@
+import { useState } from 'react';
 import './UserForm.scss'
 
 const UserForm = () => {
+    const [ newUser, setNewUser ] = useState({ name: '', email: '' });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Success!  ${newUser.name} submitted!`);
+    };
+
+    const handleNameField = (e) => {
+        setNewUser({ ...newUser, name: e.target.value });
+    };
+
+    const handleEmailField = (e) => {
+        setNewUser({ ...newUser, email: e.target.value });
+    };
+
     return(
-        <form className='user-form'>
-            <div className='form-name'>
-                <label htmlFor='name'>Name</label>
-                <input id='name' type="text" />
-            </div>
-            <div className='form-email'>
-                <label htmlFor='email'>Email</label>
-                <input id='email' type="text" />
-            </div>
-            <button className='btn form-btn' type='submit' onClick={ ()=>{} }>Submit</button>
+        <form className='user-form' onSubmit={ handleSubmit }>
+            <input
+                className='form-name'
+                value={ newUser.name }
+                onChange={ handleNameField }
+                placeholder='Name'
+            />
+            <input
+                className='form-email'
+                value={ newUser.email }
+                onChange={ handleEmailField }
+                placeholder='Email'
+            />
+            <button
+                className='btn form-btn'
+                type='submit'
+                >Submit
+            </button>
         </form>
     );
 };
