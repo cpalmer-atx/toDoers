987d84db64433d17a99d4a0595400dc7aef1476f
Chad Palmer <chad.n.palmer@gmail.com>
2022-03-28 16:55:48 -0500

    AddUser form made, add user button toggles form visibility

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -2,9 +2,17 @@ import './UserForm.scss'
 
 const UserForm = () => {
     return(
-        <div className='user-form'>
-            <h3>User Form</h3>
-        </div>
+        <form className='user-form'>
+            <div className='form-name'>
+                <label htmlFor='name'>Name</label>
+                <input id='name' type="text" />
+            </div>
+            <div className='form-email'>
+                <label htmlFor='email'>Email</label>
+                <input id='email' type="text" />
+            </div>
+            <button className='btn form-btn' type='submit' onClick={ ()=>{} }>Submit</button>
+        </form>
     );
 };
 
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import UserForm from '../UserForm/UserForm';
 import UserList from '../UserList/UserList';
 import './Users.scss'
 
 const Users = () => {
+    const [ visible, setVisible ] = useState(false);
+    const handleClick = (e) => { setVisible(!visible) }
+
     return(
         <div className='users'>
             <h2>Reserved for Users</h2>
-            <UserForm />
+            <button className='btn' onClick={ handleClick }>Add new user</button>
+            { visible && <UserForm /> }
             <UserList />
         </div>
     );
