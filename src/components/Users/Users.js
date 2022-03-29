b6cd932fa35e0eb2d7470f52a5d3df4f1661d782
Chad Palmer <chad.n.palmer@gmail.com>
2022-03-29 16:21:11 -0500

    users component now stores all users as state until db configured

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -5,14 +5,16 @@ import './Users.scss'
 
 const Users = () => {
     const [ visible, setVisible ] = useState(false);
+    const [ users, setUsers ] = useState([]);
+    
     const handleClick = (e) => { setVisible(!visible) }
 
     return(
         <div className='users'>
             <h2>Reserved for Users</h2>
             <button className='btn' onClick={ handleClick }>Add new user</button>
-            { visible && <UserForm /> }
-            <UserList />
+            { visible && <UserForm addUser={setUsers} users={users} /> }
+            <UserList users={users} />
         </div>
     );
 };
