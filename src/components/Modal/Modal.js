2d796203c08c2bf057c8004473f85a71a1452801
Chad Palmer <chad.n.palmer@gmail.com>
2022-03-31 20:15:44 -0500

    modal component created, not functional/styled yet

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,27 @@
+import './Modal.scss'
+
+const Modal = ({ viewModal, toggleModal }) => {
+
+    const closeModal = (e) => { toggleModal(!viewModal) }
+    const handleSubmit = (e) => {
+        alert('New to-do added!'); 
+        toggleModal(!viewModal)
+    }
+    return (
+        <div className='modal'>
+            <div className='modal-container'>
+                <div className='modal-header'>
+                    <button className='modal-close-btn' onClick={closeModal}> X </button>
+                </div>
+                <div className='modal-body'>
+                    <h1>Reserved for to-do input text field</h1>
+                </div>
+                <div className='modal-footer'>
+                    <button className='submit-btn' onClick={handleSubmit}>Submit</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
